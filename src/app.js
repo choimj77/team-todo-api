a5d7e5d6bd4cd4b3468a8829722d855bbfa71585
Minjun Choi <chmj041204@naver.com>
2026-02-09 15:13:19 +0900

    chore: update server app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const teamsRouter = require("./routes/teams");
 const todosRouter = require("./routes/todos");
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/health", (req, res) => res.json({ ok: true }));
 
